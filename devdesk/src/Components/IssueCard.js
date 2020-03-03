83d4a48229acd718cea1154cd003c385f9953b88
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-03-03 10:49:22 -0700

    Link to issue page in IssueCard

--- devdesk/src/Components/IssueCard.js
+++ devdesk/src/Components/IssueCard.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 import { Card } from "@material-ui/core";
+import { Link } from "react-router-dom";
 
 export default function IssueCard(props) {
   return (
     <Card>
-      <h2>{props.title}</h2>
+      <h2><Link to={`/issues/${props.id}`}>{props.title}</Link></h2>
       <div>{props.content}</div>
     </Card>
   );

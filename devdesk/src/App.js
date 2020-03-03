3161efb9f1e6ef515ba4ba7f88b665157184615e
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-03-03 10:28:59 -0700

    Add Issue page, move issues state to App component

--- devdesk/src/App.js
+++ devdesk/src/App.js
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { Route, Switch } from "react-router-dom";
 import Login from "./Components/Login";
+import Issue from "./Components/Issue.js";
 import IssueList from "./Components/IssueList";
 import Registar from "./Components/Registar";
 
 function App() {
+  const [issues, setIssues] = useState([
+    { id: 0, title: "Hello, world!", content: "I can say hello." },
+    {
+      id: 1,
+      title: "Live share doesn't work",
+      content:
+        "I installed the thing but then it says not enough stuff is installed. :/"
+    }
+  ]);
   return (
     <div className="App">
       <Switch>
-        <Route path="/issues" component={IssueList} />
+        <Route
+          path="/issues/:id"
+          render={props => <Issue {...props} issues={issues} />}
+        />
+        <Route
+          path="/issues"
+          render={props => <IssueList {...props} issues={issues} />}
+        />
         <Route path="/registar" component={Registar} />
         <Route exact path="/" component={Login} />
       </Switch>
--- devdesk/src/Components/Issue.js
+++ devdesk/src/Components/Issue.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { Container } from "@material-ui/core";
 import IssueCard from "./IssueCard.js";
+import { useParams } from "react-router-dom";
 
 export default function Queue({ issues }) {
+  const params = useParams();
+  const issue = issues[params.id];
   return (
     <Container>
-      {issues.map(issue => (
-        <IssueCard key={issue.id} {...issue} />
-      ))}
+      <IssueCard {...issue} />
     </Container>
   );
 }
